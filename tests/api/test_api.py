7c2b4c37f635c0cec8ae8c994011c1f4beb2b97a
Andrey <andrey@andreys-air.lan>
2024-06-05 18:21:35 +0300

    macbook

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,18 +1,3 @@
-import pytest
-
-
-class User:
-
-    def __init__(self) -> None:
-        self.name = 'Sergii'
-        self.second_name = 'Butenko'
-
-
-@pytest.fixture
-def user():
-    yield User()
-
-
 def test_remove_name(user):
     user.name = ''
     assert user.name == ''
