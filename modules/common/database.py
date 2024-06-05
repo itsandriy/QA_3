8e2957f06fcb53705fe63335f955a016256779fb
Andrey <andrey@andreys-air.lan>
2024-06-05 22:18:10 +0300

    macbook

---
+++ modules/common/database.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+
+class Database():
+
+    def __init__(self):
+        self.connection = sqlite3.connect('/Users/andrey/Documents/Git/QA_3' + r'/become_qa_auto.db')
+        self.cursor = self.connection.cursor()
+
+    def test_connection(self):
+        sqlite_select_Query = "SELECT sqlite_version();"
+        self.cursor.execute(sqlite_select_Query)
+        record = self.cursor.fetchall()
+        print(f"Connected successfully. SQLite Database Version is: {record}")
+
+    def get_all_users(self):
+        query = "SELECT name, address, city FROM customers"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def get_user_address_by_name(self, name):
+        query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def update_product_qnt_by_id(self, product_id, qnt):
+        query = f"UPDATE products SET quantity = {qnt} WHERE id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def select_product_qnt_by_id(self, product_id):
+        query = f"SELECT quantity FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def insert_product(self, product_id, name, description, qnt):
+        query = f"INSERT OR REPLACE INTO products (id, name, description, quantity) \
+            VALUES ({product_id}, '{name}', '{description}', {qnt})"
+        self.cursor.execute(query)
+        self.connection.commit()
+        
