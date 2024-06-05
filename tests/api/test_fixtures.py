0fe6367b990163545070ad616b3deba7c5d5e0ab
Andrey <andrey@andreys-imac.lan>
2024-06-05 15:50:09 +0300

    imac

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class User:
 
     def __init__(self) -> None:
@@ -13,17 +16,19 @@ class User:
         self.second_name = ''
 
 
-def test_change_name():
+@pytest.fixture
+def user():
     user = User()
     user.create()
 
+    yield user
+
+    user.remove()
+
+
+def test_change_name(user):
     assert user.name == 'Sergii'
-    user.remove()
 
 
-def test_change_second_name():
-    user = User()
-    user.create()
-
+def test_change_second_name(user):
     assert user.second_name == 'Butenko'
-    user.remove()
